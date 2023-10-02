cab1ac8f67fb13cdcb753de73c680f4a01d6f368
gilbert <gilbert.bageni@student.moringaschool.com>
2023-10-02 12:24:40 +0300

    initial commit

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Configure the database URI
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///superheroes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
